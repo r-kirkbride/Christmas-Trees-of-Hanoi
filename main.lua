155d5c9d69ca2c0520c06bf73da6fc5e1d66bffb
RS200E <77583841+RS200E@users.noreply.github.com>
2022-12-19 23:02:57 +0000

    Removed excess code

--- main.lua
+++ main.lua
@@ -21,7 +21,7 @@ function love.load()
     })
 
     love.keyboard.keysPressed = {}
-
+    GAME_STATE = "start"
     smallFont = love.graphics.newFont('font.ttf', 8)
     largeFont = love.graphics.newFont('font.ttf', 16)
     scoreFont = love.graphics.newFont('font.ttf', 32)
@@ -36,102 +36,35 @@ function love.load()
     
     for i=0,numDiscs,1 do
         layers[i] = Layer(200, 650-(numDiscs*30)+30*i, 75+25*i, 50, 10)
-    end
-
-    for i, v in ipairs(layers) do
-        rod1[i] = v
+        rod1[i] = i
         rod2[i] = nil
         rod3[i] = nil
     end
 
-
 end
 
---[[ function love.update(dt)
+function love.update(dt)
     if GAME_STATE == "start" then
         if love.keyboard.wasPressed('return') then
-            GAME_STATE = "serve"
-        end
-        player1Score = 0
-        player2Score = 0
-    elseif GAME_STATE == "serve" then
-        if love.keyboard.isDown('w') then
-            player1:moveUp(dt)
-        end
-        if love.keyboard.isDown('s') then
-            player1:moveDown(dt)
-        end
-
-        if love.keyboard.isDown('p') then
-            player2:moveUp(dt)
-        end
-        if love.keyboard.isDown('l') then
-            player2:moveDown(dt)
-        end
-        if love.keyboard.wasPressed('return') then
-            ball.dy = ball.dy + math.random(-50, 50)
-            if math.random(1, 2) == 1 then
-                ball.dx = -400
-            else
-                ball.dx = 400
-            end
             GAME_STATE = "play"
         end
+        score = 0
     elseif GAME_STATE == "play" then
-        if love.keyboard.isDown('w') then
-            player1:moveUp(dt)
-        end
-        if love.keyboard.isDown('s') then
-            player1:moveDown(dt)
+        a = "a"
         end
 
-        if love.keyboard.isDown('p') then
-            player2:moveUp(dt)
-        end
-        if love.keyboard.isDown('l') then
-            player2:moveDown(dt)
-        end
-
-        if ball:collides(player1) then
-            if ball.dx < 0 then
-                ball.x = ball.x + 5
-            else
-                ball.x = ball.x - 5
+        flag = true
+        for i=0,numDiscs,1 do
+            if rod3[i] ~= i then
+                flag = false
             end
-            if love.keyboard.isDown('w') or love.keyboard.isDown('s') then
-                ball.dy = ball.dy * 2
-            end
-            ball.dx = ball.dx * -1
-        elseif ball:collides(player2) then
-            if ball.dx < 0 then
-                ball.x = ball.x + 5
-            else
-                ball.x = ball.x - 5
-            end
-            if love.keyboard.isDown('p') or love.keyboard.isDown('l') then
-                ball.dy = ball.dy * 2
-            end
-            ball.dx = ball.dx * -1
         end
-
-        if ball.x < 0 then
-            GAME_STATE = "serve"
-            player2Score = player2Score + 1
-            ball:reset()
-        elseif ball.x > 432 then
-            GAME_STATE = "serve"
-            player1Score = player1Score + 1
-            ball:reset()
-        end
-        if player1Score == 10 or player2Score == 10 then
+        if flag then
             GAME_STATE = "end"
         end
 
-    end
-    ball:update(dt)
-
     love.keyboard.keysPressed = {}
-end ]]
+end
 
 function love.draw()
     push:start()
@@ -141,30 +74,9 @@ function love.draw()
         v:render()
     end
 
---[[     if GAME_STATE == "start" then
-        love.graphics.setFont(largeFont)
-        love.graphics.printf('Press Enter to start!', 0, 20, VIRTUAL_WIDTH, 'center')
-    elseif GAME_STATE == "serve" then
-        love.graphics.setFont(largeFont)
-	love.graphics.printf('Press Enter to serve!', 0, 20, VIRTUAL_WIDTH, 'center')
-	love.graphics.printf(player1Score, 0, 20, VIRTUAL_WIDTH, 'left')
-	love.graphics.printf(player2Score, 0, 20, VIRTUAL_WIDTH, 'right')
-    elseif GAME_STATE == "play" then
-	love.graphics.setFont(largeFont)
-	love.graphics.printf('Play!', 0, 20, VIRTUAL_WIDTH, 'center')
-	love.graphics.printf(player1Score, 0, 20, VIRTUAL_WIDTH, 'left')
-	love.graphics.printf(player2Score, 0, 20, VIRTUAL_WIDTH, 'right')
-    elseif GAME_STATE == "end" then
-	love.graphics.setFont(largeFont)
-	if player1Score > player2Score then
-	    love.graphics.printf('Player 1 wins!', 0, 20, VIRTUAL_WIDTH, 'center')
-	else
-	    love.graphics.printf('Player 2 wins!', 0, 20, VIRTUAL_WIDTH, 'center')
-	end
-	love.graphics.printf(player1Score, 0, 20, VIRTUAL_WIDTH, 'left')
-	love.graphics.printf(player2Score, 0, 20, VIRTUAL_WIDTH, 'right')
-    end]]
-
+    if GAME_STATE == "end" then
+        love.graphics.printf('Game Over', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
     push:finish()
 end
 
